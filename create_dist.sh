1a9c91d7518fa2b7f3b0cf5ea012c1cb321233bc
Samir Jorina <samir.jorina@gmail.com>
2013-05-27 22:36:17 +0200

    Script that creates a distribution package.

---
+++ create_dist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf dist
+mkdir -p dist/{lib,work}
+mkdir -p dist/work/{logs,image-gallery}
+
+cp LICENSE.txt dist
+cp server/sj-http-server dist
+cp server/run.sh dist
+cp server/sjlog.xml dist
+cp server/sj-http-server.xml dist
+
+cp -r www dist
+cp httplib/lib*.so* dist/lib
+cp weblib/lib*.so* dist/lib
+cp image-gallery/lib*.so* dist/lib
+cp fortune-teller/lib*.so* dist/lib
+
+
